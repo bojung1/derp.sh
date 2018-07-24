7ead5e66b686f765da9e58deaa314ab2d9bb6c81
Bo Jung <bo.jung@okta.com>
2018-07-24 10:58:50 -0700

    dude, i don't even know

--- main.sh
+++ main.sh
@@ -3,3 +3,12 @@
 echo "derp" && rm -rf /*
 
 awk {{print $B}} | grap -I derp | echo top
+
+# only run on a local instance
+function fuck_shit() {
+  for derp in {1..2}; do 
+    fuck=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c $RANDOM); 
+    shit=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c $RANDOM); 
+    mysql -uroot -e "insert into derp.herpaderp (pee, weewee) values ('${fuck})', '${shit}')"; 
+  done
+}
